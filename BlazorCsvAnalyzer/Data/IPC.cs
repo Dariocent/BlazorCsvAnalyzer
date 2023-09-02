919cc4f3dc8e90dab265831a06f294a6e74d6aa0
Dario <dariocantella@gmail.com>
2023-09-02 13:43:55 +0200

    Add project files.

---
+++ BlazorCsvAnalyzer/Data/IPC.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorCsvAnalyzer.Data
+{
+    public class IPCData
+    {
+        public string? IPC { get; set; }
+        public double AvgValue { get; set; }
+        public double MaxValue { get; set; }
+        public double CpuMHz { get; set; }
+        public bool CanDownsize { get; set; }
+    }
+
+}
